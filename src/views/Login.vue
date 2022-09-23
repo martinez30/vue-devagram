9ac617dc3976ef1af11a6abb0b18e10c15fcdbb5
Paulo Martins <paulosm.dev@gmail.com>
2022-09-23 13:40:07 -0300

    styles login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,40 +6,58 @@ export default defineComponent({
             login: '',
             senha: '',
             loading: false,
-            error: ''
+            error: '',
+            inputLoginFocus: false,
+            inputSenhaFocus: false
         }
     },
     methods: {
         efetuarLogin() {
-            if(!this.login || !this.senha){
+            if (!this.login || !this.senha) {
                 this.error = 'Favor preencher o formulario'
                 return;
             }
 
             alert(`Login efetuado ${this.login}, ${this.senha}`)
+        },
+        setFocusLogin(v: boolean) {
+            this.inputLoginFocus = v;
+        },
+        setFocusSenha(v: boolean) {
+            this.inputSenhaFocus = v;
         }
     }
 })
 </script>
 
 <template>
-    <div>
-        <img src="../assets/images/logo.svg" alt="Logo Devagram">
+    <div class="container-publico">
+        <img src="../assets/images/logo.svg" alt="Logo Devagram" class="logo">
         <form action="">
-            <p v-if="error">{{error}}</p>
-            <div>
+            <p v-if="error" class="error">{{error}}</p>
+            <div class="input" :class="{ focus: inputLoginFocus }">
                 <img src="../assets/images/login.svg" alt="Insira o login">
-                <input type="text" v-model="login" />
+                <input type="text" v-model="login" placeholder="Email" 
+                    @focus="setFocusLogin(true)"
+                    @blur="setFocusLogin(false)" 
+                />
             </div>
-            <div>
+            <div class="input" :class="{ focus: inputSenhaFocus }">
                 <img src="../assets/images/senha.svg" alt="Insira a senha">
-                <input type="password" v-model="senha" />
+                <input type="password" v-model="senha" placeholder="Senha" 
+                    @focus="setFocusSenha(true)"
+                    @blur="setFocusSenha(false)" 
+                />
             </div>
             <button @click.enter.prevent="efetuarLogin">Login</button>
-            <div>
+            <div class="link">
                 <p>Não possui uma conta?</p>
                 <a>Faça seu cadastro agora!</a>
             </div>
         </form>
     </div>
 </template>
+
+<style lang="scss" src="@/assets/styles/paginaPublica.scss">
+
+</style>
