ebb1c8ada4bcdb9da79aabedab0b739c8176c02f
Paulo Martins Dev <paulosm.dev@gmail.com>
2022-10-01 11:55:01 -0300

    footer and background body

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,34 @@
 import { defineComponent } from "vue";
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import Feed from "../components/Feed.vue";
+import { FeedServices } from "@/services/FeedServices";
 
-export default defineComponent({ components: { Header, Footer } });
+const feedServices = new FeedServices();
+
+export default defineComponent({
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async mounted() {
+    try {
+      const resultado = await feedServices.getFeedPrincipal();
+
+      if (resultado && resultado.data) {
+        this.posts = resultado.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  components: { Header, Footer, Feed },
+});
 </script>
 
 <template>
-    <Header />
-    <Footer />
+  <Header />
+  <Feed :posts="posts" />
+  <Footer />
 </template>
