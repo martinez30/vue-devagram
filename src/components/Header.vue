af590a17ae365f9d45bd6b15923e4f06b101b7e2
Paulo Martins Dev <paulosm.dev@gmail.com>
2022-10-01 11:08:21 -0300

    header step 1.1

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      resultado: [],
+      inputFocus: false
+    };
+  },
+  methods: {
+    setFocus(v: boolean){
+      this.inputFocus = v;
+    }
+  }
+});
+</script>
+
+<template>
+  <header class="container-header">
+    <div class="principal">
+      <img src="@/assets/images/logo_horizontal.svg" alt="Logo Devagram" />
+      <div class="group">
+        <div class="pesquisa" :class="{focus: inputFocus}">
+          <img
+            src="@/assets/images/navbar/search.svg"
+            alt="pesquisa"
+            layout="fill"
+            class="icon"
+          />
+          <input type="text" placeholder="Pesquisar" @focus="setFocus(true)" @blur="setFocus(false)" />
+        </div>
+        <!-- Navegacao -->
+      </div>
+    </div>
+    <div class="resultado" v-if="resultado.length > 0"></div>
+  </header>
+</template>
+
+<style lang="scss" src="@/assets/styles/header.scss" />
