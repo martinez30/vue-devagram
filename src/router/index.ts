0f1a8384517b3709e50523f1e14d38587ab700b0
Paulo Martins Dev <paulosm.dev@gmail.com>
2022-10-01 17:01:38 -0300

    redirect to edit profile screen

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,54 +1,57 @@
-import { useAccessTokenStore } from '@/stores/accessToken'
-import CadastroVue from '@/views/Cadastro.vue'
-import HomeVue from '@/views/Home.vue'
-import LoginVue from '@/views/Login.vue'
-import UsuarioVue from '@/views/Usuario.vue'
-import PerfilVue from '@/views/Perfil.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { useAccessTokenStore } from "@/stores/accessToken";
+import CadastroVue from "@/views/Cadastro.vue";
+import HomeVue from "@/views/Home.vue";
+import LoginVue from "@/views/Login.vue";
+import UsuarioVue from "@/views/Usuario.vue";
+import PerfilVue from "@/views/Perfil.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import EditarPerfilVue from "@/views/EditarPerfil.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeVue,
-      meta: { rotaPrivada: true }
+      meta: { rotaPrivada: true },
     },
     {
-      path: '/',
-      name: 'login',
+      path: "/",
+      name: "login",
       component: LoginVue,
-      props: true
+      props: true,
     },
     {
-      path: '/cadastro',
-      name: 'cadastro',
-      component: CadastroVue
+      path: "/cadastro",
+      name: "cadastro",
+      component: CadastroVue,
     },
     {
-      path: '/usuario/:id',
-      name: 'usuario',
+      path: "/usuario/:id",
+      name: "usuario",
       component: UsuarioVue,
-      meta: { rotaPrivada: true }
     },
     {
-      path: '/perfil',
-      name: 'perfil',
+      path: "/perfil",
+      name: "perfil",
       component: PerfilVue,
-      meta: { rotaPrivada: true }
-    }
-  ]
-})
+    },
+    {
+      path: "/editar",
+      name: "editar",
+      component: EditarPerfilVue,
+    },
+  ],
+});
 
 router.beforeEach((to, from) => {
   const store = useAccessTokenStore();
-  if (to.name !== 'login' && to.meta.rotaPrivada && !store.estaAutenticado) {
-    return { name: 'login' }
+  if (to.name !== "login" && to.meta.rotaPrivada && !store.estaAutenticado) {
+    return { name: "login" };
+  } else if (to.name === "cadastro" && store.estaAutenticado) {
+    return { name: "home" };
   }
-  else if (to.name === 'cadastro' && store.estaAutenticado) {
-    return { name: 'home' }
-  }
-})
+});
 
-export default router
+export default router;
---
+++ src/views/EditarPerfil.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+import Feed from "../components/Feed.vue";
+import { FeedServices } from "@/services/FeedServices";
+import HeaderPerfil from "../components/HeaderPerfil.vue";
+import { UsuarioServices } from "@/services/UsuarioServices";
+import HeaderAcoes from "../components/HeaderAcoes.vue";
+
+const feedServices = new FeedServices();
+const userServices = new UsuarioServices();
+
+export default defineComponent({
+  data() {
+    return {
+      posts: [],
+      usuario: {} as any,
+    };
+  },
+  components: { Header, Footer, Feed, HeaderPerfil, HeaderAcoes },
+});
+</script>
+
+<template>
+  <Header :hide="true" />
+  <HeaderAcoes :isRightIcon="true" :showRight="true" title="Editar perfil" />
+  <Feed :posts="posts" :temCabecalho="true" />
+  <Footer />
+</template>
